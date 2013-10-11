2ab45fed7e703247260592870ddc05d2a4a1e93c
Conar Welsh <conar@Conars-iMac.local>
2013-10-11 15:04:55 -0600

    add route names

--- app/routes.php
+++ app/routes.php
@@ -5,22 +5,34 @@ Route::get('/', function()
 	return View::make('index');
 });
 
-Route::get('equipment', function()
-{
-	return View::make('equipment');
-});
+Route::get('equipment', array(
+	'as' => 'equipment',
+	function()
+	{
+		return View::make('equipment');
+	}
+));
 
-Route::get('shop', function()
-{
-	return View::make('shop');
-});
+Route::get('shop', array(
+	'as' => 'shop',
+	function()
+	{
+		return View::make('shop');
+	}
+));
 
-Route::get('contact', function()
-{
-	return View::make('contact');
-});
+Route::get('contact', array(
+	'as' => 'contact',
+	function()
+	{
+		return View::make('contact');
+	}
+));
 
-Route::get('about', function()
-{
-	return View::make('about');
-});
+Route::get('about', array(
+	'as' => 'about',
+	function()
+	{
+		return View::make('about');
+	}
+));
